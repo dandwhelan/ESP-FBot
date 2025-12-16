f571bd27d0bb5c597471153f383e20296361a631
ylianst <ylianst@gmail.com>
2025-12-16 08:23:51 -0800

    Charge Threshold Range to 10%, #10

--- components/fbot/fbot.cpp
+++ components/fbot/fbot.cpp
@@ -481,7 +481,7 @@ void Fbot::control_light(bool state) {
 void Fbot::set_threshold_charge(float percent) {
   // Convert percentage to permille (multiply by 10)
   uint16_t value = static_cast<uint16_t>(percent * 10);
-  if (value < 600) { value = 600; }  // Minimum 600
+  if (value < 100) { value = 100; }  // Minimum 100
   if (value > 1000) { value = 1000; }  // Maximum 1000
   this->send_control_command(REG_THRESHOLD_CHARGE, value);
 }
